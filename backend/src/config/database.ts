46fbf07bb0b5bcf4fa8401be040210f422aa7118
Raissa de Oliveira Souza <rosouza_23@outlook.com>
2026-08-17 22:28:10 -0300

    feat: configura sequelize, migrations e modelo user

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,29 +1,28 @@
-// Importa a classe Sequelize da biblioteca de ORM 
-import { Sequelize } from "sequelize";
-// Importa a biblioteca dotenv para conseguir ler as variáveis protegidas do arquivo .env
+import { Options, Sequelize } from "sequelize";
 import dotenv from 'dotenv'
 
 // Executa a configuração do dotenv para carregar as variáveis de ambiente no processo do Node.js
 dotenv.config();
 
-// Cria e exporta a instância de conexão com o banco de dados usando o Sequelize
+const isSSL = process.env.DB_SSL === 'true';
+const sequelizeOptions: Options = {
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions : isSSL 
+    ? {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+    : {}
+}
+
 export const sequelize = new Sequelize(
     process.env.DB_NAME || 'postgres',
     process.env.DB_USER || 'postgres',
     process.env.DB_PASSWORD || '',
-    {
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT || '5432', 10),
-        dialect: 'postgres',
-        logging: false,
-        // Configurações específicas exigidas pelo protocolo de segurança do banco
-        dialectOptions: {
-            ssl: {
-                // Obriga o uso de conexão segura/criptografada (obrigatório para bancos em nuvem como o Supabase)
-                require: true,
-                // Evita bloqueios de conexão caso o certificado SSL do servidor seja auto-assinado ou não verificado
-                rejectUnauthorized: false
-            }
-        }
-    }
+    sequelizeOptions
 )
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import express, {Request, Response} from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { sequelize } from './config/database'
+import { User } from './models/User'
 
 // Carrega as variáveis de ambiente do arquivo .env para dentro do process.env
 dotenv.config()
@@ -20,8 +22,34 @@ app.get('/api/health', (req: Request, res: Response) => {
     res.status(200).json({
         status: 'OK',
         mensagem: 'Servidor backend rodando com sucesso',
-        timeStamp: new Date().toISOString() // Data e hora exatas da requisição
-    })
+        timestamp: new Date().toISOString()
+    });
+});
+
+// Rota para cadastrar novo usuário
+app.post('/api/users', async (req: Request, res:Response) => {
+    try {
+        const {nome, email, senha_hash} = req.body;
+            if (!nome || !email || !senha_hash){
+                return res.status(400).json({erro: 'nome, email, senha_hash são obrigatórios'})
+            } 
+        const novoUsuario = await User.create({nome, email, senha_hash});
+        return res.status(201).json(novoUsuario)
+    } catch(error: any){
+        return res.status(500).json({erro: 'Erro ao cadastrar o usuário.', detalhe: error.mensage})
+    }
+});
+
+// Rota para listar todos os usuários 
+app.get('/api/users', async (req:Request, res:Response) => {
+    try {
+        const usuarios = await User.findAll({
+            attributes: ['id', 'nome', 'email', 'createdAt']
+        });
+        return res.status(200).json(usuarios);
+    } catch(error: any){
+        return res.status(500).json({erro: 'Erro ao listar os usuários.', detalhe: error.mensage})
+    }
 })
 
 // Faz o servidor "ouvir" as requisições na porta definida
